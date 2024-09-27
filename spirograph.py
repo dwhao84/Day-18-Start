59f96f24a329fdcaecca634bc6430ff44927dba1
Dawei Hao <dwsamurai84@gmail.com>
2024-09-27 21:29:39 +0800

    Finish the spirograph.py project into the Day 18's learning.

--- spirograph.py
+++ spirograph.py
@@ -22,7 +22,19 @@ for i in range(36):
     t.left(10)
     t.speed("fast")
     
-screen.exitonclick() # screen運行完之後，視窗關閉。
+########### Angela的解法:
+# 設定size_of_gap的數值為所畫的圈數。
+# 並將整個功能寫成def，將再把size_of_gap的數值傳進去。
+def draw_spirograph(size_of_gap):
+    for i in range(int(360 / size_of_gap)):
+        t.color(random_color())
+        # This method is used to draw a circle with a given radius
+        t.circle(100)
+        # setheading(angle): Sets the orientation of the turtle to angle
+        t.setheading(t.heading() +  size_of_gap)
+        
+# screen運行完之後，視窗關閉。
+screen.exitonclick()
 
 
 
